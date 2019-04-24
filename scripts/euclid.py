0700e5ddbeca0ba34d6802a00ece25cfbd7643c1
juanfresia <juanmanuelfresia@gmail.com>
2019-04-23 22:26:45 -0300

    Pretty print
    Signed-off-by: juanfresia <juanmanuelfresia@gmail.com>

--- scripts/euclid.py
+++ scripts/euclid.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python3
 
-def euclid(a, b):
+def euclid(a, b, show=False):
     r0 = [a, 1, 0]
     r1 = [b, 0, 1]
-    print(r0)
-    print(r1)
+    if show: print(r0)
+    if show: print(r1)
     while r1[0] != 0:
         q = r0[0] // r1[0]
         rn = [x - q*y for x, y in zip(r0, r1)]
 
         r0 = r1
         r1 = rn
-        print(r1)
+        if show: print(r1)
 
     return r0
 
@@ -24,10 +24,12 @@ def inverse(a, n):
         return r[1]
     return 0
 
-print(euclid(1759, 550))
-print(gcd(1759, 550))
-print(inverse(1759, 550))
+print("euclid(1759, 550)")
+print(euclid(1759, 550, show=True))
 
+print()
+print("gcd(1759, 550) = ", gcd(1759, 550))
 
-
+print()
+print("inverse_550(1759) =", inverse(1759, 550))
 
