944e7a4aff2ec3dc1ab6a12d91642e6c59e08ca2
Juan Manuel Fresia <jfresia@medallia.com>
2019-06-13 01:27:36 -0300

    Network basics and openssl.cnf fix
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- ipsec-vm/provision.sh
+++ ipsec-vm/provision.sh
@@ -5,9 +5,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y build-essential zip
-#apt-get install -y lubuntu-desktop
 
-#apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+apt-get install -y lubuntu-desktop
+apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
 cp -r /vagrant/files/crypto /crypto
 
@@ -15,11 +15,9 @@ apt-get install -y git
 apt-get install -y wireshark
 apt-get install -y libreswan
 
-
 # This enables the use of scp with password authentication
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
-
 
 # Enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
@@ -29,3 +27,24 @@ sysctl -w net.ipv4.conf.all.send_redirects=0
 sysctl -w net.ipv4.conf.all.accept_redirects=0
 sysctl -w net.ipv4.conf.default.send_redirects=0
 sysctl -w net.ipv4.conf.default.accept_redirects=0
+
+# Update known hosts
+cat <<EOF | sudo tee /etc/hosts
+10.5.1.2    crypto-1
+10.5.1.1    crypto-2
+10.5.2.1    crypto-3
+10.5.2.2    crypto-4
+192.168.5.41 crypto-2
+192.168.5.42 crypto-3
+EOF
+
+## Add routes for hosts through GW
+NODE_NAME=$(hostname)
+if [[ ${NODE_NAME} == "crypto-1" ]]; then
+    ip route add 10.5.2.0/24 via 10.5.1.1
+fi
+if [[ ${NODE_NAME} == "crypto-4" ]]; then
+    ip route add 10.5.1.0/24 via 10.5.2.1
+fi
+
+
